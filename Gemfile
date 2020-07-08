fea1417be8e7aba4641dac4d844a48f6c197d4af
Naoya Fujio <naofudiwo@gmail.com>
2020-07-08 01:34:12 +0000

    add page_nation

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'kaminari', '~> 0.17.0'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.8'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_074148) do
+ActiveRecord::Schema.define(version: 2020_07_07_124916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_074148) do
     t.datetime "updated_at", null: false
     t.date "dead_line", null: false
     t.string "status", default: "", null: false
+    t.integer "priority", default: 0, null: false
     t.index ["status"], name: "index_tasks_on_status"
   end
 
