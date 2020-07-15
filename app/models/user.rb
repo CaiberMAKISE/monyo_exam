94a0cdc5e5d0eee224e44df04eabe5d78cd5a9ac
Naoya Fujio <naofudiwo@gmail.com>
2020-07-15 04:42:51 +0000

    fix user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_destroy :admin_destroy_check
-    before_update :admin_destroy_check
+    before_update :admin_edit_check
     validates :name, presence: true, length: { maximum: 30 }
     validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
@@ -11,6 +11,12 @@ class User < ApplicationRecord
     private
     def admin_destroy_check
         if  admin == true
+            user_count = User.where(admin: true).count
+            throw(:abort) if user_count <= 1
+        end
+    end
+    def admin_edit_check
+        if  admin == false
             user_count = User.where(admin: true).count
             throw(:abort) if user_count <= 1
         end
